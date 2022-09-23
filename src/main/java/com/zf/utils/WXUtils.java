3356bea4833cccbecb4cb1c3cb6f27cb0c329911
amureux <2293667568@qq.com>
2022-09-23 22:53:53 +0800

    第18次提交 修改 APP_ID 修改SECRET

--- src/main/java/com/zf/utils/WXUtils.java
+++ src/main/java/com/zf/utils/WXUtils.java
@@ -17,6 +17,8 @@ import java.util.Map;
 public class WXUtils {
 
     /**
+     * 吴松：wx2026ed61968ede2f   7c76c90ed8a4390220f7fd73bb6c7654
+     *
      * WXLogin:
      * <p>
      * APP_ID: wxafcffece6679f01a
@@ -25,8 +27,8 @@ public class WXUtils {
      * GRANT_TYPE: authorization_code
      */
 
-    private static final String APP_ID = "wx2026ed61968ede2f";
-    private static final String SECRET = "7c76c90ed8a4390220f7fd73bb6c7654";
+    private static final String APP_ID = "wxafcffece6679f01a";
+    private static final String SECRET = "83b297e0e7412cf259b6e0346ed5d1f6";
     private static final String JS_CODE = "023G9A00018tyO1pFT300RY3XL3G9A03";
     private static final String GRANT_TYPE = "authorization_code";
 
