b3070f3c3b15234ce1aac57ce4a1729511be630f
pd <1340420664@qq.com>
2022-10-24 08:42:08 +0800

    员工管理搜索Vo

---
+++ src/main/java/com/zf/domain/vo/UserQueryVo.java
@@ -0,0 +1,42 @@
+package com.zf.domain.vo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author pd
+ * DateTime: 2022/10/21 15:48
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserQueryVo {
+
+  @ApiModelProperty(value = "当前页",dataType = "int")
+  private Integer pageNum;
+
+  @ApiModelProperty(value = "每页条数",dataType = "int")
+  private Integer pageSize;
+
+  @ApiModelProperty(value = "员工编号",dataType = "int")
+  private String userId;
+
+  @ApiModelProperty(value = "员工岗位",dataType = "String")
+  private String userJob;
+
+  @JsonFormat(pattern = "yyyy-MM-dd")
+  @ApiModelProperty(value = "开始时间",dataType = "String")
+  private String startTime;
+
+  @JsonFormat(pattern = "yyyy-MM-dd")
+  @ApiModelProperty(value = "结束时间",dataType = "String")
+  private String endTime;
+}
